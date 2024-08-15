a866eee91373f5c4f1befc74b9c886e26f8992d7
Salyiel <162126755+Salyiel@users.noreply.github.com>
2024-08-15 19:37:51 +0300

    Complete assignment

--- server/app.py
+++ server/app.py
@@ -1,5 +1,3 @@
-# server/app.py
-#!/usr/bin/env python3
 
 from flask import Flask, make_response
 from flask_migrate import Migrate
@@ -23,14 +21,33 @@ def index():
     )
     return response
 
+
 @app.route('/pets/<int:id>')
 def pet_by_id(id):
     pet = Pet.query.filter(Pet.id == id).first()
-    response_body = f'<p>{pet.name} {pet.species}</p>'
 
-    response = make_response(response_body, 200)
+    if pet:
+        response_body = f'<p>{pet.name} {pet.species}</p>'
+        response_status = 200
+    else:
+        response_body = f'<p>Pet {id} not found</p>'
+        response_status = 404
 
+    response = make_response(response_body, response_status)
     return response
+
+
+@app.route('/species/<string:species>')
+def pet_by_species(species):
+    pets = Pet.query.filter_by(species=species).all()
+
+    size = len(pets)  # all() returns a list so we can get length
+    response_body = f'<h2>There are {size} {species}s</h2>'
+    for pet in pets:
+        response_body += f'<p>{pet.name}</p>'
+    response = make_response(response_body, 200)
+    return response
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
